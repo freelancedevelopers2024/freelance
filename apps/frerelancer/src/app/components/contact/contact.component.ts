99eb67a186c0232d1950ac133bd53a4e83589837
Toni Pavlov Yakimov <you@example.com>
2024-06-25 19:04:12 +0200

    [WIP] Implement contact

--- apps/frerelancer/src/app/components/contact/contact.component.ts
+++ apps/frerelancer/src/app/components/contact/contact.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {}
+export class ContactComponent implements OnInit {
+  private fb = inject(FormBuilder)
+  private _snackBar = inject(MatSnackBar)
+  
+  contactForm!: FormGroup;
+
+  ngOnInit(){
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.maxLength(500)]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
+      this.contactForm.reset();
+      this._snackBar.open('Thanks for your message, we will replay as soon as possible :)','Ok',{
+        duration: 3000
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
